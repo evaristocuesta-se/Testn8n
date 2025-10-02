23ab8641729522f0ef2d19ab0cfc1bbc62819e14
Evaristo Cuesta <SESA639503@se.com>
2025-10-02 13:48:44 +0200

    Change variable name to userName

--- Testn8n/Program.cs
+++ Testn8n/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string NAME = "n8n";
+string userName = "n8n";
 
-Console.WriteLine($"Hello, {NAME}!");
+Console.WriteLine($"Hello, {userName}!");
