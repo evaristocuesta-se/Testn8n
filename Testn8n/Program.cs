749091664d497e28757272f52fe92c2dde3babc3
Evaristo Cuesta <SESA639503@se.com>
2025-10-03 10:13:53 +0200

    Add feature

--- Testn8n/Program.cs
+++ Testn8n/Program.cs
@@ -3,11 +3,11 @@ string? userName = Console.ReadLine();
 Console.WriteLine($"Hello, {userName}!");
 
 Console.Write("Enter first number: ");
-string? a = Console.ReadLine();
+string? A = Console.ReadLine();
 Console.Write("Enter second number: ");
 string? b = Console.ReadLine();
 
-if (int.TryParse(a, out int num1) && int.TryParse(b, out int num2))
+if (int.TryParse(A, out int num1) && int.TryParse(b, out int num2))
 {
     int sum = Testn8n.Math.Add(num1, num2);
     Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
